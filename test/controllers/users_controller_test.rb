0c8767b8ca5c6726529a1d4a6664f6dd0d8d0c44
Anna Baranova <asbaranova1@gmail.com>
2021-12-14 18:21:29 -0500

    add test to index page

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -11,5 +11,10 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Sign up | #{@base_title}"
   end
-  
+
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
 end
