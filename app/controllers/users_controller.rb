cae7e42baff76fc07195a83c6319403e89d576d8
Anna Baranova <asbaranova1@gmail.com>
2021-12-15 12:28:46 -0500

    add destroy user" q

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user,   only: :destroy
   
   def new
     @user = User.new
@@ -41,6 +42,18 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id]).destroy
+    flash[:success] = "#{@user.name} deleted"
+    redirect_to users_path
+  end
+
+  # Confirm an admin user
+  def admin_user
+      redirect_to(root_url) unless current_user.admin?
+  end
+
+
 
   private
 
@@ -60,5 +73,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
   end
+
 
 end
