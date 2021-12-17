6e9357ac3cfc2cfddd2c631cac8afff53c4ef3e3
Anna Baranova <asbaranova1@gmail.com>
2021-12-16 23:03:33 -0500

    update feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,10 +49,12 @@ class User < ApplicationRecord
     remember_digest || remember
   end
 
-  def feed 
-    Micropost.where('user_id = ?', id)
+  def feed
+    part_of_feed = "relationships.follower_id = :id or microposts.user_id = :id"
+    Micropost.left_outer_joins(user: :followers)
+             .where(part_of_feed, { id: id }).distinct
+             .includes(:user, image_attachment: :blob)
   end
-
    # Follows a user.
    def follow(other_user)
     following << other_user unless self == other_user
--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Rails2
     config.action_dispatch.default_headers = {
       'X-Frame-Options' => ''
     }
+
+    # Include the authenticity token in remote forms.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
